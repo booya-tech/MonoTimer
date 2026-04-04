c6fdf4608864f09ad07de95b150b7703a6de685d
Boo Pannachai <panachais@phillip.co.th>
2026-04-04 13:53:12 +0700

    fix: code refactoring, and add keyboard dismiss

--- FocusSpace/Core/Extensions/View.swift
+++ FocusSpace/Core/Extensions/View.swift
@@ -5,4 +5,10 @@
 //  Created by Panachai Sulsaksakul on 4/4/26.
 //
 
-import Foundation
+import SwiftUI
+
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
